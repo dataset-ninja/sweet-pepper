418c87025cf1369af077ff818d58e06416c6f862
Ars Vladimirov <grokhael@gmail.com>
2023-08-02 16:13:18 +0600

    HORIZONTAL_GRID_ROWS

--- src/options.py
+++ src/options.py
@@ -17,8 +17,8 @@ POSTER_TITLE: str = None
 
 ###############################################################
 #### * Set up visualization params for HorizontalGrid class ###
-HORIZONTAL_GRID_ROWS: int = 3
-HORIZONTAL_GRID_COLS: int = 6
+HORIZONTAL_GRID_ROWS: int = 2
+HORIZONTAL_GRID_COLS: int = 3
 HORIZONTAL_GRID_IS_DETECTION_TASK: bool = IS_DETECTION_TASK
 ###############################################################
 
